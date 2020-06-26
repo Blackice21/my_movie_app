73c0ee7806dc1e4ddaab2add292daabeeee4af71
Marcus <marcusmorrow319@gmail.com>
2020-06-26 06:28:30 -0500

    final touches

--- cp_django_movie_app/urls.py
+++ cp_django_movie_app/urls.py
@@ -18,9 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('', include('movies.urls')),
-=======
     path('', include('movies.urls'))
->>>>>>> step_0_start
 ]
